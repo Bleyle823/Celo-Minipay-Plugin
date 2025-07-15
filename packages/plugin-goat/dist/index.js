efa579277cc1d43a9d6de275f33eebbeffe35271
Bleyle823 <bennybleylle09@gmail.com>
2025-07-15 22:36:58 +0300

    refined-2

--- packages/plugin-goat/dist/index.js
+++ packages/plugin-goat/dist/index.js
@@ -1,9 +1,8 @@
 // src/actions.ts
 import { getOnChainTools } from "@goat-sdk/adapter-vercel-ai";
-//import { kim } from "@goat-sdk/plugin-kim";
+import { PEPE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
+import { uniswap } from "@goat-sdk/plugin-uniswap";
 import { sendETH } from "@goat-sdk/wallet-evm";
-import {  PEPE, USDC, erc20 } from "@goat-sdk/plugin-erc20";
-import { uniswap} from "@goat-sdk/plugin-uniswap";
 import { etherscan } from "@goat-sdk/plugin-etherscan";
 import {
   generateText,
@@ -25,30 +24,30 @@ async function getOnChainActions(wallet) {
     wallet,
     // 2. Configure the plugins you need to perform those actions
     plugins: [
-      sendETH(), 
-      erc20({ tokens: [USDC,PEPE
-
-      // Optimism Network
-      //     {
-      //     decimals: 18,
-      //     symbol: "OP",
-      //     name: "Optimism",
-      //     chains: {
-      //         "1": {
-      //             contractAddress: "0x4200000000000000000000000000000000000042",
-      //         },
-      //     },
-      // },
-  
-  ] }), 
+      sendETH(),
+      erc20({ tokens: [
+        USDC,
+        PEPE
+        // Optimism Network
+        //     {
+        //     decimals: 18,
+        //     symbol: "OP",
+        //     name: "Optimism",
+        //     chains: {
+        //         "1": {
+        //             contractAddress: "0x4200000000000000000000000000000000000042",
+        //         },
+        //     },
+        // },
+      ] }),
       uniswap({
-          baseUrl: process.env.UNISWAP_BASE_URL,
-          apiKey: process.env.UNISWAP_API_KEY
+        baseUrl: process.env.UNISWAP_BASE_URL,
+        apiKey: process.env.UNISWAP_API_KEY
       }),
       etherscan({
-          apiKey: process.env.ETHERSCAN_API_KEY
-        }),
-  ]
+        apiKey: process.env.ETHERSCAN_API_KEY
+      })
+    ]
   });
   return actionsWithoutHandler.map((action) => ({
     ...action,
@@ -70,10 +69,10 @@ function getActionHandler(actionName, actionDescription, tools) {
         context,
         tools,
         maxSteps: 10,
-        //Uncomment to see the log each tool call when debugging
-        onStepFinish: (step) => {
-            console.log(step.toolResults);
-        },
+        // Uncomment to see the log each tool call when debugging
+        // onStepFinish: (step) => {
+        //     console.log(step.toolResults);
+        // },
         modelClass: ModelClass.LARGE
       });
       const response = composeResponseContext(result, currentState);
@@ -193,20 +192,34 @@ async function generateResponse(runtime, context) {
 
 // src/wallet.ts
 import { viem } from "@goat-sdk/wallet-viem";
-import { createWalletClient, http } from "viem";
+import { createWalletClient, formatEther, http, parseEther } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
-import {  optimism} from "viem/chains";
-var chain =  optimism;
-function getWalletClient(getSetting) {
+import {
+  optimismSepolia
+} from "viem/chains";
+import { publicActionsL2 } from "viem/op-stack";
+async function getWalletClient(getSetting) {
   const privateKey = getSetting("EVM_PRIVATE_KEY");
   if (!privateKey) return null;
   const provider = getSetting("EVM_PROVIDER_URL");
   if (!provider) throw new Error("EVM_PROVIDER_URL not configured");
   const wallet = createWalletClient({
     account: privateKeyToAccount(privateKey),
-    chain,
+    chain: optimismSepolia,
     transport: http(provider)
+  }).extend(publicActionsL2());
+  const fee = await wallet.estimateTotalFee({
+    account: privateKeyToAccount(privateKey),
+    to: "0x70997970c51812dc3a010c7d01b50e0d17dc79c8",
+    value: parseEther("1"),
+    chain: optimismSepolia
   });
+  const feeInEther = formatEther(fee);
+  try {
+    console.log(`Estimated Total Fee: ${feeInEther} ETH`);
+  } catch (error) {
+    console.error("Error estimating the transaction fee:", error);
+  }
   return viem(wallet);
 }
 function getWalletProvider(walletClient) {
@@ -218,7 +231,7 @@ function getWalletProvider(walletClient) {
         return `EVM Wallet Address: ${address}
 Balance: ${balance} ETH`;
       } catch (error) {
-        console.error("Error in EVM wallet provider:", error);
+        console.error("Error in  wallet provider:", error);
         return null;
       }
     }
@@ -227,11 +240,11 @@ Balance: ${balance} ETH`;
 
 // src/index.ts
 async function createGoatPlugin(getSetting) {
-  const walletClient = getWalletClient(getSetting);
+  const walletClient = await getWalletClient(getSetting);
   const actions = await getOnChainActions(walletClient);
   return {
-    name: "[GOAT] Onchain Actions",
-    description: "Optimism integration plugin",
+    name: "Superchain Plugin",
+    description: "Superchain Plugin built with Op Stack",
     providers: [getWalletProvider(walletClient)],
     evaluators: [],
     services: [],
